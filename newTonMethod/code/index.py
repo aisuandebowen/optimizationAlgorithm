c9cc12fb4fff795d8eedb617a4485cd6446adeaa
Boen <1836140285@qq.com>
2025-03-17 10:21:45 +0800

    test: modify some logs in newTonMethod

--- newTonMethod/code/index.py
+++ newTonMethod/code/index.py
@@ -3,4 +3,5 @@ import newTonMethod
 xList = [0,1]
 finalXList,finalFn = newTonMethod.run(xList)
 
-print(finalXList,finalFn)
+print(f'初始点：{xList}, 收敛点：{finalXList}')
+print(f'最优解:{finalFn}')
--- newTonMethod/code/newTonMethod.py
+++ newTonMethod/code/newTonMethod.py
@@ -17,20 +17,14 @@ def run(xList):
     gradFn,normalGradFn = _getInitData(xList)
     d = None
     print(gradFn,normalGradFn)
-    print('begin==============')
     # 迭代
     while normalGradFn>=epsilon:
         hesseFn = tools.getHesseFn(xList) # Hesses矩阵
         invHesseFn = np.linalg.inv(hesseFn) # 求Hesse逆矩阵
-        print(f'hesseFn：${hesseFn}')
 
         d = -np.dot(invHesseFn,gradFn) # 方向
         xList = xList + d # 更新xList
         gradFn, normalGradFn = _getInitData(xList) # 更新相关数据
-        print(f'x：${xList}')
-        print(f'gradFn：${gradFn}')
-        print(f'normalGradFn：${normalGradFn}')
-        print('=============================')
 
     # 终止
     fn = tools.getFn(xList)
